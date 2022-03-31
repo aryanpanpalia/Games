93771370e07eca62cf028642258b09d9af20ece3
Aryan Panpalia <aryanpanpalia@gmail.com>
2022-03-31 16:28:56 -0400

    New connections now handled in background
    Server now checks for new connections in a thread and then creates a new thread to deal with whether they create or join a room.
    
    Also, now that all happens in the background while other code is running (here, filler code that outputs the filled and unfilled rooms is what runs).

--- Chess/server.py
+++ Chess/server.py
@@ -1,5 +1,7 @@
 import random
 import socket
+import threading
+import time
 
 
 class Room:
@@ -16,45 +18,57 @@ unfilled = []
 filled = []
 rooms = []
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 55555))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('0.0.0.0', 55555))
+
+def handle_new_connection(csock, addr):
+    print(f"Connection from {addr} established!")
+    csock.send(bytes("Welcome to Chess!", "utf-8"))
+    csock.send(bytes("Do you want to create or join a room: ", "utf-8"))
+
+    response = csock.recv(1024).decode("utf-8")
+
+    if response.lower() == "create":
+        new_room_code = random.choice([x for x in range(1, 10000) if x not in [room.code for room in rooms]])
+        new_room = Room(csock, new_room_code)
+        unfilled.append(new_room)
+        rooms.append(new_room)
+        print(f"Created room {new_room}")
+        csock.send(bytes(f"Your room code is {new_room_code}", "utf-8"))
+    elif response.lower() == "join":
+        # prompt them for join code then set them as player 2
+        csock.send(bytes("Enter the room code: ", "utf-8"))
+        response = csock.recv(1024).decode("utf-8")
+
+        for room in unfilled:
+            if int(response) == room.code:
+                room.player2 = csock
+
+                unfilled.remove(room)
+                filled.append(room)
+                print(f"Filled room {room}")
+
+                room.player1.send(bytes("Someone has joined the room!", "utf-8"))
+                csock.send(bytes(f"Joined room!", "utf-8"))
+
+
+def handle_new_connections():
     s.listen()
 
     # IT TAKES A BIT FOR THE WHOLE ITERATION OF THE LOOP TO RUN
     # IF SOMEONE ELSE TRIES TO CONNECT WHILE THE ITERATION IS BEING RUN, THEY MIGHT BE VERY DELAYED OR IGNORED
     while True:
         clientsocket, address = s.accept()
-        print(f"Connection from {address} established!")
-        clientsocket.send(bytes("Welcome to Chess!", "utf-8"))
-        clientsocket.send(bytes("Do you want to create or join a room: ", "utf-8"))
-
-        response = clientsocket.recv(1024).decode("utf-8")
-
-        if response.lower() == "create":
-            new_room_code = random.choice([x for x in range(1, 10000) if x not in [room.code for room in rooms]])
-            new_room = Room(clientsocket, new_room_code)
-            unfilled.append(new_room)
-            rooms.append(new_room)
-            print(f"Created room {new_room}")
-            clientsocket.send(bytes(f"Your room code is {new_room_code}", "utf-8"))
-        elif response.lower() == "join":
-            # prompt them for join code then set them as player 2
-            clientsocket.send(bytes("Enter the room code: ", "utf-8"))
-            response = clientsocket.recv(1024).decode("utf-8")
-
-            for room in unfilled:
-                if int(response) == room.code:
-                    room.player2 = clientsocket
-
-                    unfilled.remove(room)
-                    filled.append(room)
-                    print(f"Filled room {room}")
-
-                    room.player1.send(bytes("Someone has joined the room!", "utf-8"))
-                    clientsocket.send(bytes(f"Joined room!", "utf-8"))
-
-        print(unfilled)
-        print(filled)
+        # handle_new_connection(clientsocket, address)
+        handle_new_connection_thread = threading.Thread(target=handle_new_connection, args=(clientsocket, address))
+        handle_new_connection_thread.start()
 
 
+handle_new_connections_thread = threading.Thread(target=handle_new_connections)
+handle_new_connections_thread.start()
+
+while True:
+    print(unfilled)
+    print(filled)
+    time.sleep(1)
