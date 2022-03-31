ada244045f848c3a92fd7f1ac7d494f1fbcf7d63
Aryan Panpalia <aryanpanpalia@gmail.com>
2022-03-31 17:05:13 -0400

    Minor modifications
    Cleaned up code and added comments to client.py to help understand what is going on.
    
    In server.py, the room is set to play once filled.

--- Chess/client.py
+++ Chess/client.py
@@ -1,29 +1,38 @@
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((socket.gethostname(), 55555))
 
-    # Welcome message
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((socket.gethostname(), 55555))
 
-    # Whether joining or creating a room
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
-    response = input()
-    s.send(bytes(response, "utf-8"))
-
-    if response.lower() == "create":
+        # Welcome message
         msg = s.recv(1024)
         print(msg.decode("utf-8"))
-    elif response.lower() == "join":
+
+        # Message prompting for create or join
         msg = s.recv(1024)
-        print(msg.decode("utf-8"))
-        response = input()
+        response = input("Enter whether you want to join or create a room: ")
         s.send(bytes(response, "utf-8"))
 
+        if response.lower() == "create":
+            # Message giving room code
+            msg = s.recv(1024)
+            print(msg.decode("utf-8"))
+            print("Waiting for someone to join...")
+        elif response.lower() == "join":
+            # Message asking for room code
+            msg = s.recv(1024)
+            response = input("Enter your room code: ")
+            s.send(bytes(response, "utf-8"))
 
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
+        # Message stating that someone/you joined the room
+        msg = s.recv(1024)
+        print(msg.decode("utf-8"))
 
-    msg = s.recv(1024)
+        # Message from inside Room.play()
+        msg = s.recv(1024)
+        print(msg.decode("utf-8"))
+
+
+if __name__ == '__main__':
+    main()
--- Chess/server.py
+++ Chess/server.py
@@ -3,12 +3,27 @@ import socket
 import threading
 import time
 
+import model
+from model import WHITE, BLACK, GameRules, CHECKMATE, GAME_IN_PLAY, STALEMATE
+
 
 class Room:
     def __init__(self, host, code):
-        self.player1 = host
-        self.player2 = None
-        self.code = code
+        self.player1: socket.socket = host
+        self.player2: socket.socket = None
+        self.code: int = code
+        self.game: model.Game = None
+
+    def play(self):
+        self.player1.send(bytes("LET THE GAME BEGIN", "utf-8"))
+        self.player2.send(bytes("LET THE GAME BEGIN", "utf-8"))
+
+        # Filler code
+        p1i = self.player1.recv(1024).decode("utf-8")
+        p2i = self.player2.recv(1024).decode("utf-8")
+
+        print(p1i)
+        print(p2i)
 
     def __repr__(self):
         return f"{self.player1}; {self.player2}; {self.code}"
@@ -23,7 +38,6 @@ s.bind(('0.0.0.0', 55555))
 
 
 def handle_new_connection(csock, addr):
-    print(f"Connection from {addr} established!")
     csock.send(bytes("Welcome to Chess!", "utf-8"))
     csock.send(bytes("Do you want to create or join a room: ", "utf-8"))
 
@@ -34,7 +48,6 @@ def handle_new_connection(csock, addr):
         new_room = Room(csock, new_room_code)
         unfilled.append(new_room)
         rooms.append(new_room)
-        print(f"Created room {new_room}")
         csock.send(bytes(f"Your room code is {new_room_code}", "utf-8"))
     elif response.lower() == "join":
         # prompt them for join code then set them as player 2
@@ -47,20 +60,19 @@ def handle_new_connection(csock, addr):
 
                 unfilled.remove(room)
                 filled.append(room)
-                print(f"Filled room {room}")
 
                 room.player1.send(bytes("Someone has joined the room!", "utf-8"))
                 csock.send(bytes(f"Joined room!", "utf-8"))
+
+                room_thread = threading.Thread(target=room.play)
+                room_thread.start()
 
 
 def handle_new_connections():
     s.listen()
 
-    # IT TAKES A BIT FOR THE WHOLE ITERATION OF THE LOOP TO RUN
-    # IF SOMEONE ELSE TRIES TO CONNECT WHILE THE ITERATION IS BEING RUN, THEY MIGHT BE VERY DELAYED OR IGNORED
     while True:
         clientsocket, address = s.accept()
-        # handle_new_connection(clientsocket, address)
         handle_new_connection_thread = threading.Thread(target=handle_new_connection, args=(clientsocket, address))
         handle_new_connection_thread.start()
 
