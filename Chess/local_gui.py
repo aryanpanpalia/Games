e53acc41b414a949b32a85a0533e67b1c9cdf14d
Aryan Panpalia <aryanpanpalia@gmail.com>
2022-05-30 22:41:44 -0400

    Add draw and resign buttons to local_gui

--- Chess/local_gui.py
+++ Chess/local_gui.py
@@ -66,11 +66,45 @@ def draw_move_list(x, y, width, height, win, game):
                 win.blit(font.render(group[1].to_algebraic_notation(), True, (200, 200, 200)), (x + 150, y + 3 + MOVE_LIST_LINE_HEIGHT * i))
 
 
+def draw_draw_button(x, y, width, height, win):
+    font = pg.font.SysFont("bahnschrift", 20)
+
+    draw_rect = pg.Rect(x, y, width, height)
+    pg.draw.rect(win, (30, 30, 30), draw_rect)
+
+    # text should start at start_x + 0.5 * box_width - 0.5 * text_width, start_y + 0.5 * box_height - 0.5 * text_height so that its centered with respect to function parameters
+    if not DRAW_OFFERED:
+        # text has width of 48 and height of 24
+        win.blit(font.render(f"Draw", True, (200, 200, 200)), (x + 0.5 * width - 24, y + 0.5 * height - 12))
+    elif TURNS_SINCE_DRAW_OFFERED == 0:
+        # text has width of 118 and height of 24
+        win.blit(font.render(f"Draw offered", True, (200, 200, 200)), (x + 0.5 * width - 59, y + 0.5 * height - 12))
+    elif TURNS_SINCE_DRAW_OFFERED == 1:
+        # text has width of 115 and height of 25
+        win.blit(font.render(f"Accept draw", True, (200, 200, 200)), (x + 0.5 * width - 57.5, y + 0.5 * height - 12.5))
+    elif TURNS_SINCE_DRAW_OFFERED == 2:
+        # text has width of 128 and height of 25
+        win.blit(font.render(f"Draw rejected", True, (200, 200, 200)), (x + 0.5 * width - 64, y + 0.5 * height - 12.5))
+
+
+def draw_resign_buttons(x, y, width, height, win):
+    font = pg.font.SysFont("bahnschrift", 20)
+
+    resign_rect = pg.Rect(x, y, width, height)
+    pg.draw.rect(win, (30, 30, 30), resign_rect)
+
+    # text should start at start_x + 0.5 * box_width - 0.5 * text_width, start_y + 0.5 * box_height - 0.5 * text_height so that its centered with respect to function parameters
+    # text has width of 61 and height of 25 and needs to be centered in a box with (start_x, start_y, box_width, box_height) = (x + width / 2 + 5, y, width / 2 - 5, height)
+    win.blit(font.render(f"Resign", True, (200, 200, 200)), (x + 0.5 * width - 30.5, y + 0.5 * height - 12.5))
+
+
 def render(win, game, perspective=WHITE):
     win.fill((60, 60, 60))
 
     draw_board(BOARD_OFFSET_X, BOARD_OFFSET_Y, BOARD_WIDTH, BOARD_HEIGHT, win, game, perspective)
     draw_move_list(MOVE_LIST_OFFSET_X, MOVE_LIST_OFFSET_Y, MOVE_LIST_WIDTH, MOVE_LIST_HEIGHT, win, game)
+    draw_draw_button(DRAW_BUTTON_OFFSET_X, DRAW_BUTTON_OFFSET_Y, DRAW_BUTTON_WIDTH, DRAW_BUTTON_HEIGHT, win)
+    draw_resign_buttons(RESIGN_BUTTON_OFFSET_X, RESIGN_BUTTON_OFFSET_Y, RESIGN_BUTTON_WIDTH, RESIGN_BUTTON_HEIGHT, win)
 
     pg.display.update()
 
@@ -83,7 +117,7 @@ def main():
     game = Game()
     running = True
 
-    global square_to_move_from, MOVE_LIST_SCROLL
+    global square_to_move_from, MOVE_LIST_SCROLL, DRAW_OFFERED, TURNS_SINCE_DRAW_OFFERED
 
     square_to_move_from = None
     square_to_move_to = None
@@ -138,6 +172,16 @@ def main():
 
                         if square_to_move_from and not square_to_move_to and square_to_move_from != square:
                             square_to_move_to = square
+                    elif DRAW_BUTTON_OFFSET_X < pos[0] < DRAW_BUTTON_OFFSET_X + DRAW_BUTTON_WIDTH and DRAW_BUTTON_OFFSET_Y < pos[1] < DRAW_BUTTON_OFFSET_Y + DRAW_BUTTON_HEIGHT:
+                        if not DRAW_OFFERED:
+                            DRAW_OFFERED = True
+                            TURNS_SINCE_DRAW_OFFERED = 0
+                        elif TURNS_SINCE_DRAW_OFFERED == 1:
+                            print("Draw!")
+                            running = False
+                    elif RESIGN_BUTTON_OFFSET_X < pos[0] < RESIGN_BUTTON_OFFSET_X + RESIGN_BUTTON_WIDTH and RESIGN_BUTTON_OFFSET_Y < pos[1] < RESIGN_BUTTON_OFFSET_Y + RESIGN_BUTTON_HEIGHT:
+                        print(f"{'Black' if turn == WHITE else 'White'} won due to {'White' if turn == WHITE else 'Black'}'s resignation.")
+                        running = False
 
         if square_to_move_from is not None and square_to_move_to is not None:
             # Promotion
@@ -170,6 +214,7 @@ def main():
                     print(f'Checkmate! {"WHITE" if turn == WHITE else "BLACK"} won!')
                     game.moves[-1].mate = True
                     running = False
+                    DRAW_OFFERED = False
                 elif game.check_if_game_ended() == STALEMATE:
                     print("Stalemate!")
                     running = False
@@ -181,6 +226,13 @@ def main():
                         game.moves[-1].check = True
 
                 turn *= -1
+
+                if DRAW_OFFERED:
+                    if TURNS_SINCE_DRAW_OFFERED < 2:
+                        TURNS_SINCE_DRAW_OFFERED += 1
+                    else:
+                        TURNS_SINCE_DRAW_OFFERED = -1
+                        DRAW_OFFERED = False
 
             square_to_move_from = None
             square_to_move_to = None
@@ -212,10 +264,22 @@ if __name__ == '__main__':
     MOVE_LIST_OFFSET_X = BOARD_OFFSET_X + BOARD_WIDTH + 20
     MOVE_LIST_OFFSET_Y = 50
     MOVE_LIST_WIDTH = 440
-    MOVE_LIST_HEIGHT = 800
+    MOVE_LIST_HEIGHT = 730
     NUM_TURNS_IN_MOVE_LIST = 25
     MOVE_LIST_LINE_HEIGHT = MOVE_LIST_HEIGHT / NUM_TURNS_IN_MOVE_LIST
     MOVE_LIST_SCROLL = 0
+
+    DRAW_BUTTON_OFFSET_X = BOARD_OFFSET_X + BOARD_WIDTH + 20
+    DRAW_BUTTON_OFFSET_Y = MOVE_LIST_OFFSET_Y + MOVE_LIST_HEIGHT + 20
+    DRAW_BUTTON_WIDTH = 210
+    DRAW_BUTTON_HEIGHT = 50
+    DRAW_OFFERED = False
+    TURNS_SINCE_DRAW_OFFERED = -1
+
+    RESIGN_BUTTON_OFFSET_X = DRAW_BUTTON_OFFSET_X + DRAW_BUTTON_WIDTH + 20
+    RESIGN_BUTTON_OFFSET_Y = MOVE_LIST_OFFSET_Y + MOVE_LIST_HEIGHT + 20
+    RESIGN_BUTTON_WIDTH = 210
+    RESIGN_BUTTON_HEIGHT = 50
 
     piece_images = {image[1]: pg.transform.smoothscale(pg.image.load(rss_path(f'assets/{image}')), (100, 100)) for image in os.listdir(rss_path("assets"))}
 
